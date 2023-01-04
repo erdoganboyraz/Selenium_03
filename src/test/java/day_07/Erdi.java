fa6885ce94653e521e09fd0d3954ee3d5ad8fe54
erdogan boyraz <boyraz9898@gmail.com>
2023-01-04 23:56:38 +0300

    04.01.2023

--- src/test/java/day_07/Erdi.java
+++ src/test/java/day_07/Erdi.java
@@ -3,32 +3,41 @@ package day_07;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 public class Erdi extends TestBase {
     @Test
     public void test01() throws InterruptedException {
-        // 2. Navigate to url 'http://automationexercise.com'
-        // 3. Verify that home page is visible successfully
-        driver.get("http://automationexercise.com");
-        String actualTitle = driver.getTitle();
-        System.out.println(actualTitle);
-        String expectedTitle = "Automation Exercise";
-        Assert.assertEquals(expectedTitle,actualTitle);
-        // 4. Click on 'Products' button
-        driver.findElement(By.xpath("//*[@class='material-icons card_travel']")).click();
-        // 5. Verify user is navigated to ALL PRODUCTS page successfully
-        String actualTitle2 = driver.getTitle();
-        String expectedTitle2 = "Automation Exercise ";
-        System.out.println("actualTitle2 = " + actualTitle2);
-        Assert.assertEquals(expectedTitle2,actualTitle2);
-        // 6. The products list is visible
-        // 7. Click on 'View Product' of first product
-        // 8. User is landed to product detail page
-        // 9. Verify that detail detail is visible: product name, category, price, availability, condition, brand
-          }
+
+        // https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/ sitesine gidin
+        driver.get("https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/");
+        // ikinci emojiye tıklayın
+        WebElement ıframe = driver.findElement(By.xpath("//*[@id='emoojis']"));
+        driver.switchTo().frame(ıframe);
+        driver.findElement(By.xpath("(//*[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]")).click();
+        // tüm ikinci emoji öğelerini tıklayın
+
+        //for (WebElement w:emojiler) {
+        //    w.click();
+        //}
+        // parent iframe e geri donun
+
+        //driver.navigate().refresh();
+        // formu doldurun,(Formu istediğiniz metinlerle doldurun)
+        //driver.findElement(By.xpath("//*[@id='text']")).sendKeys("Erol", Keys.TAB,"Evren");
+
+
+        //  apply button a basin
+
+
+    }
 }
 
